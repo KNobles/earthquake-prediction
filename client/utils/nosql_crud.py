1251da5dbaa668407f0986dec2719f7d6b7f25ad
KNobles <nobles-k@hotmail.com>
2023-01-20 11:51:40 +0100

    added requirements client

--- client/utils/nosql_crud.py
+++ client/utils/nosql_crud.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pandas as pd
 from dotenv import load_dotenv
 from azure.cosmos import CosmosClient, ContainerProxy, DatabaseProxy, PartitionKey
 
@@ -15,37 +16,6 @@ def connect(endpoint:str, key:str) -> ContainerProxy:
     container_name = "tweets_db"
     partition_key = PartitionKey(path="/id")
     database:DatabaseProxy = client.create_database_if_not_exists(id=database_name)
-    container:ContainerProxy = database.create_container_if_not_exists(id=container_name, partition_key=partition_key)
-    return container
-
-# Inserts tweet to DB
-def insert_tweet(tweet_dict:dict, tweet_id:int):
-    container = connect(endpoint=endpoint, key=key)
-    if (tweet_id != 0):
-        json_object = tweet_dict.collect()[0][0]
-        tweet_dumps = json.dumps(eval(json_object))
-        tweet_json = json.loads(tweet_dumps)
-        container.create_item(tweet_json)
-
-import os
-from dotenv import load_dotenv
-from azure.cosmos import CosmosClient, ContainerProxy, PartitionKey
-import json
-import re
-import pandas as pd
-
-load_dotenv()
-
-endpoint = os.environ["NOSQL_ENDPOINT"]
-key = os.environ["NOSQL_KEY"]
-
-# Connect to the "raw_tweets" database and "tweets" container
-def connect(endpoint:str, key:str) -> ContainerProxy:
-    client = CosmosClient(endpoint, key)
-    database_name = "raw_tweets"
-    container_name = "tweets_db"
-    partition_key = PartitionKey(path="/id")
-    database = client.create_database_if_not_exists(id=database_name)
     container:ContainerProxy = database.create_container_if_not_exists(id=container_name, partition_key=partition_key)
     return container
 
