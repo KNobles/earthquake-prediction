4593dd6e452b06ba55c6e5536f5022096a4d8304
tanialemos <tanialemosribeiro@gmail.com>
2023-01-24 11:42:07 +0100

    ongoing metadata extraction

--- model/metadata_extraction.py
+++ model/metadata_extraction.py
@@ -2,17 +2,21 @@ import pandas as pd
 import spacy
 from postgresql_crud import insert_data
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="MyApp")
+import datetime
+from dateutil import parser
 
 """
     This script will extract Named Entities from a tweet text.
     The entities will be processed and saved to the DB as latitude, longitude, magnitude, date and time.
 """
 
-test_tweet = "USGS reports a M2.4 earthquake, 39 km W of Mentone, Texas on 1/17/23 @ 4:49:57 UTC https://t.co/HA6XJ8XzF6 #earthquake"
-test_entities = [('ORG', 'USGS'), ('CARDINAL', 'M2.4'), ('QUANTITY', '39 km'), ('GPE', 'Mentone'), ('GPE', 'Texas'), ('DATE', '1/17/23'), ('TIME', '4:49:57 UTC')]
-
+#test_tweet = "USGS reports a M2.4 earthquake, 39 km W of Mentone, Texas on 1/17/23 @ 4:49:57 UTC https://t.co/HA6XJ8XzF6 #earthquake"
+#test_entities = [('ORG', 'USGS'), ('CARDINAL', 'M2.4'), ('QUANTITY', '39 km'), ('GPE', 'Mentone'), ('GPE', 'Texas'), ('DATE', '1/17/23'), ('TIME', '4:49:57 UTC')]
+geolocator = Nominatim(user_agent="MyApp")
 nlp = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
+
+# read example tweets from .csv to populate db
+df = pd.read_csv("../../becode/earthquake-prediction-fork/files/earthquake_tweets.csv")
 
 def extract_entities_from_tweet_text(text:str) -> list:
     entities = []
@@ -37,9 +41,21 @@ def get_date_and_time_from_entities(entities):
     if entities != []:
         for label, text in entities:
             if label.startswith('DATE'):
-                date = text
+                try:
+                    date_object = parser.parse(text)
+                    date_formatted = date_object.strftime('%Y-%m-%d')
+                    date = date_formatted
+                except Exception as e:
+                    date = None
+                    print(e)
             if label.startswith('TIME'):
-                time = text
+                try:
+                    time_object = parser.parse(text)
+                    extracted_time = time_object.strftime("%H:%M:%S")
+                    time = extracted_time
+                except Exception as e:
+                    time = None
+                    print(e)
     return date, time
 
 # function to get the geo coordinates as a list of tuples: latitude, longitude
@@ -72,5 +88,24 @@ def insert_metadata_in_db(test_tweet):
     insert_data(metadata)
     print("metadata inserted!")
 
-insert_metadata_in_db(test_tweet)
+def insert_metadata_in_db_from_csv(id, text):
+    entities = extract_entities_from_tweet_text(text)
+    location = get_location_from_entities(entities)
+    coordinates = get_geo_coordinates(location)
+    date, time = get_date_and_time_from_entities(entities)
+    metadata = {
+        "id" : id,
+        "text" : text,
+        "lat" : coordinates[0][1] if coordinates != [] else None,
+        "long" : coordinates[1][1] if coordinates != [] else None,
+        "date" : date if date != "" else None, # yyyy-mm-dd
+        "time" : time if time != "" else None # hh:mm:ss
+    }
+    try:
+        insert_data(metadata)
+        print(f"metadata inserted!")
+    except Exception as e:
+        print(f"error saving tweet id: {id}. {e}")
 
+df.apply(lambda row : insert_metadata_in_db_from_csv(row['tweet_id'],
+                     row['tweet_text']), axis = 1)
--- model/postgresql_crud.py
+++ model/postgresql_crud.py
@@ -2,6 +2,7 @@ import os
 import time
 import random
 import psycopg2
+import pandas as pd
 
 from dotenv import load_dotenv
 
@@ -18,41 +19,56 @@ conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(hos
 conn = psycopg2.connect(conn_string)
 print("Connection established")
 
-cursor = conn.cursor()
 
 # TODO Create a Sample table which has the same columns but only ~500 items
 # TODO Create tweet_info table which will have the data processed by the ML sent live
 # Create a table
 def create_table():
+    cursor = conn.cursor()
     cursor.execute("""
 CREATE TABLE IF NOT EXISTS tweet_info(
     tweet_id VARCHAR PRIMARY KEY,
     translated_text TEXT, 
     geo_lat DECIMAL(7,5), 
-    get_lng DECIMAL(8,5),
+    geo_lng DECIMAL(8,5),
     magnitude DECIMAL(3,1),
     date DATE,
     time TIME);""")
     print("Finished creating table")
     conn.commit()
+    cursor.close()
 
 def delete_content():
+    cursor = conn.cursor()
     cursor.execute("TRUNCATE tweet_info;")
     conn.commit()
+    cursor.close()
 
 def insert_data(data:dict):
+    cursor = conn.cursor()
     id = data["id"]
     text = data["text"]
     lat = data["lat"]
+    long = data["long"]
     date = data["date"] # yyyy-mm-dd
     time = data["time"] # hh:mm:ss
-    query = """INSERT INTO tweet_info (tweet_id, translated_text, geo_lat, date, time) 
-    VALUES (%s,%s, %s, %s, %s);"""
-    cursor.execute(query, (id, text, lat, date, time))
+    query = """INSERT INTO tweet_info (tweet_id, translated_text, geo_lat, geo_lng, date, time) 
+    VALUES (%s,%s, %s, %s, %s, %s) ON CONFLICT(tweet_id) DO NOTHING;"""
+    cursor.execute(query, (id, text, lat, long, date, time))
     conn.commit()
     cursor.close()
-    conn.close()
 
+# TODO to delete, just for testing purposes
+def get_all_table():
+    cursor = conn.cursor()
+    cursor.execute("""SELECT COUNT(*) FROM tweet_info;""")
+    result = cursor.fetchall()
+    print(result)
+    conn.commit()
+    cursor.close()
 
+#get_all_table()
+
+#create_table()
 
 # insert_data({"id":'hjkdchjkd', "text":"henloo", "lat":88.0003, "date":"2023-01-20", "time":"14:22:37"})
