9d9f9146e2b479125d74131ba2f9d20b3a5f7823
Antoine Vanneste <113119888+AVanneste@users.noreply.github.com>
2023-01-25 15:31:46 +0100

    added selenium imports

--- client/utils/nosql_crud.py
+++ client/utils/nosql_crud.py
@@ -6,8 +6,11 @@ from json import loads
 import re
 import pandas as pd
 import requests
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv()
 
